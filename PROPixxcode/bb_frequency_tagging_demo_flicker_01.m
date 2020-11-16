79f021a7cbb2de14b6eea30e8e7b6a5f46f2f777
y.cao@uke.de <y.cao@uke.de>
2020-11-16 13:33:44 +0100

    x

--- PROPixxcode/bb_frequency_tagging_demo_flicker_01.m
+++ PROPixxcode/bb_frequency_tagging_demo_flicker_01.m
@@ -2,7 +2,7 @@
 % Function
 % If any questions, please contact a.zhigalov@bham.ac.uk
 %-------------------------------------------------------------------------------
-function bb_frequency_tagging_demo_flicker_01(f1, f2)
+function bb_frequency_tagging_demo_flicker_01()
 
 clc;
 
@@ -10,7 +10,7 @@ try sca
 catch
 end
 
-padWidth = 40;
+padWidth = 60;
 picWidth = 250;
 
 % set up parameters
@@ -21,18 +21,24 @@ PsychDefaultSetup(2);
 % screen info and colors
 screens = Screen('Screens');
 scrNumber = max(screens); % draw to the external screen if avaliable
-gray = 0.4;
+gray = 0;
 % create a screen window
-[window, ~] = PsychImaging('OpenWindow', scrNumber, gray);
+[window, rect] = PsychImaging('OpenWindow', scrNumber, gray);
 Screen('Flip', window);
 % screen and text parameters
 [scrWidth, scrHeight] = Screen('WindowSize', window);
 
+center = [rect(3)/2, rect(4)/2];
+base = [center(1)-padWidth/2, center(2)-padWidth/2, center(1)+padWidth/2, center(2)+padWidth/2];
+
 ifi = Screen('GetFlipInterval', window);
 FR = Screen('NominalFrameRate', window); % monitor frame rate
 
-FR = 120;
-ifi = 1/FR;
+%FR = 120;
+%ifi = 1/FR;
+
+FR
+ifi
 
 % my monitor bug
 % if FR > 80, FR = 120; else, FR = 60; end
@@ -53,12 +59,22 @@ Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 tag_setup_projector('open', 1);
 tag_setup_projector('set', 1);
 
+
+freq = [51, 63, 78, 87, 92, 108, 120, 133, 152];
+
+for whichf = 1:length(freq)
+
+f1 = freq(whichf);
+f2 = f1;
+
 % generate tagging signal
-d2 = 1.5; % duration of tagging signal
+d2 = 1.0; % duration of tagging signal
 d6 = 0.3; % break
 D2 = round(FR * d2 * 12); % 12 is the Propixx multiplier for gray scale
 D6 = round(FR * d6 * 12);
+
 [xColor1d0, xColor1d1] = tag_get_tagging_signal(d2 + d6, D2 + D6, f1, f2);
+size(xColor1d0)
 xColor3d0 = reshape(xColor1d0, 4, 3, []);
 xColor3d1 = reshape(xColor1d1, 4, 3, []);
 xColor3d = {xColor3d0, xColor3d1};
@@ -67,21 +83,39 @@ D6 = D6 / 12;
 
 vbl = Screen('Flip', window);
 
+%Set up some stimulus characteristics
+dotRadius = 10;
+
+%Create some positions based on the regular display
+center = [rect(3)/2, rect(4)/2];
+radius = 0;
+positions=[center(1), center(2)-radius;...            %top
+           center(1)+radius, center(2);...            %right
+           center(1), center(2)+radius;...            %bottom
+           center(1)-radius, center(2)];              %left
+
 % loop
-for trial = 1:100
+for trial = 1
   for d = 1:(D2 + D6) % VBL frames
         fColor = xColor3d{1}(:, :, d);
         hColor = xColor3d{2}(:, :, d);
         if d < (D2 + 1) % tagging
-          for i = 1:4
+          for quadrant = 1:4
+            % position = positions(quadrant, :);
+            i = quadrant;
             Screen('DrawTexture', window, padFrame, [], padLRect{i}, 0, [], 1, fColor(i, :));
-            Screen('DrawTexture', window, padFrame, [], padRRect{i}, 0, [], 1, hColor(i, :));
+            % [x,y] = convertToQuadrant(position, rect, quadrant);
+            % Screen('FillOval', window, fColor(quadrant, :), [x-dotRadius, y-dotRadius, x+dotRadius, y+dotRadius]);
+            % Screen('DrawTexture', window, padFrame, [], padRRect{i}, 0, [], 1, hColor(i, :));
           end
         end
         vbl = Screen('Flip', window, vbl + 0.5 * ifi);
   end
   vbl = Screen('Flip', window, vbl + 0.5 * ifi); 
 end
+
+end
+
 
 % close screen
 Screen('CloseAll');
@@ -95,6 +129,28 @@ end % end
 %-------------------------------------------------------------------------------
 % Function
 %-------------------------------------------------------------------------------
+
+function [x,y] = convertToQuadrant(position, displaySize, quad)
+%This scales an x, y position into a specific quadrant of the screen    
+scale = 0.5;
+
+switch quad
+    case 1; xOffset = 0; yOffset = 0;
+    case 2; xOffset = displaySize(3)/2; yOffset = 0; 
+    case 3; xOffset = 0; yOffset = displaySize(4)/2;
+    case 4; xOffset = displaySize(3)/2; yOffset = displaySize(4)/2;
+end
+    
+x = (position(1)*scale)+xOffset;
+y = (position(2)*scale)+yOffset;
+
+end
+
+
+
+
+
+
 function [picFrame, padFrame] = tag_get_pictures(window, picWidth, padWidth)
 
 picFrame = uint8(zeros(picWidth, picWidth));
