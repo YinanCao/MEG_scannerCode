bcd0e9ea1ca6681c8a14a1e1563b63368be17571
Yinan Cao <yinan.cao@psy.ox.ac.uk>
2020-11-18 10:42:51 +0100

    upload

--- sensory_localizer_FTag/Set_Vars.m
+++ sensory_localizer_FTag/Set_Vars.m
@@ -141,5 +141,5 @@ xCoords                       = [-fixCrossDimPix fixCrossDimPix 0 0];
 yCoords                       = [0 0 -fixCrossDimPix fixCrossDimPix];
 allCoords                     = [xCoords; yCoords];
 lineWidthPix                  = round(info.pix_per_deg*Gabor.Fixation_dot_deg);%6 pix
-fix_rect                      = round([-fixCrossDimPix -lineWidthPix./2 fixCrossDimPix lineWidthPix./2]./2);
+fix_rect                      = floor([-fixCrossDimPix -lineWidthPix./2 fixCrossDimPix lineWidthPix./2]./2);
 
